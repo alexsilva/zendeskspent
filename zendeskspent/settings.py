f1e245186c0059bd71d9212dc280b53b0e398446
alex <geniofuturo@gmail.com>
2015-03-05 17:19:54 -0300

    Configurando xadmin como interface padrão.

--- zendeskspent/settings.py
+++ zendeskspent/settings.py
@@ -35,13 +35,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contracts',
+    'xadmin',
+    'crispy_forms',
 )
 
 MIDDLEWARE_CLASSES = (
--- zendeskspent/urls.py
+++ zendeskspent/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+import xadmin
+xadmin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'zendeskspent.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       
+    url(r'^admin/', include(xadmin.site.urls)),
 )
