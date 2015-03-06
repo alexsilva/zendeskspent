e804838483225edf84cf912bfc16d6a28764a5d7
alex <geniofuturo@gmail.com>
2015-03-06 17:22:28 -0300

    Adicionando método para a api v2 zendesk.

--- zendeskspent/__init__.py
+++ zendeskspent/__init__.py
@@ -1,5 +1,16 @@
+# coding=utf-8
 from __future__ import absolute_import
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
+
+import zendesk
+
+# método da api zendesk faltando na biblioteca (zendesk)
+zendesk.mapping_table_v2['list_tickets'] = {
+    'path': '/views/{{view_id}}/tickets.json',
+    'valid_params': ('view_id',),
+    'status': 200,
+    'method': 'GET'
+}
