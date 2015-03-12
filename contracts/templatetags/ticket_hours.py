033ab8c8848368e1d06bea5d8c2f6f9291ea236c
alex <geniofuturo@gmail.com>
2015-03-12 18:05:07 -0300

    Monstrando informaçoes do consumo de horas.

--- contracts/templatetags/ticket_hours.py
+++ contracts/templatetags/ticket_hours.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django import template
 
 register = template.Library()
@@ -7,3 +8,15 @@ __author__ = 'alex'
 @register.simple_tag(takes_context=True)
 def load_hours(context, ticket, contract):
     return ticket.get_field_value(contract.company.spent_hours_external) or 0.0
+
+
+
+@register.simple_tag(takes_context=True)
+def calc_hours_remainder(context, contract, data):
+    """Total de horas restantes para todo o período"""
+    total_hours = contract.hours
+    hours = []
+    for key, objects in data.iteritems():
+        hours.append(sum([(float(ticket.get_field_value(contract.company.spent_hours_external) or 0.0))
+                          for ticket in objects]))
+    return total_hours - sum(hours)
