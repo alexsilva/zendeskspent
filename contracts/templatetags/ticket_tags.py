85681f65f1372ff6f4dfb5815bb8346d93b661b3
alex <geniofuturo@gmail.com>
2015-03-13 10:39:41 -0300

    Criando link para o id do ticket.

--- contracts/templatetags/ticket_tags.py
+++ contracts/templatetags/ticket_tags.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from django import template
+from django.conf import settings
+
 from remotesyc.models import Ticket
 
 register = template.Library()
@@ -9,7 +11,6 @@ __author__ = 'alex'
 @register.simple_tag(takes_context=True)
 def load_hours(context, ticket, contract):
     return ticket.get_field_value(contract.company.spent_hours_external) or 0.0
-
 
 
 @register.simple_tag(takes_context=True)
@@ -31,3 +32,8 @@ def calc_hours_spent(context, contract, data):
 @register.simple_tag(takes_context=True)
 def resolve_status(context, ticket):
     return Ticket.STATUS.choices_as_dict()[ticket.status]
+
+
+@register.filter()
+def resolve_id_url(ticket):
+    return "{0}/tickets/{1.pk}".format(settings.ZENDESK_BASE_URL, ticket)
